b7b2845aad1423baad60884e2cec2aa0cfdf1976
wujizhishui007 <1185266842@qq.com>
2019-04-30 16:46:01 +0800

    时间复杂度

--- algorithm/selection_sort.php
+++ algorithm/selection_sort.php
@@ -3,23 +3,32 @@
  * 选择排序
  * @authors Joker (njfupl@foxmail.com)
  * @date    2017-10-22 11:48:15
+ *
+ * 一种简单直观的排序算法。它的工作原理是每一次从待排序的数据元素中选出最小（或最大）的一个元素，存放在序列的起始位置，
+ * 然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * 以此类推，直到全部待排序的数据元素排完。
+ * 选择排序是不稳定的排序方法。
+ *
+ * 时间复杂度: O(n*n)
+ *
  * 实现数组的升序
  */
 echo "<pre>";
 
 function  selectionSort(&$arr){
 	for ($i=0; $i < count($arr)-1; $i++) { 
-		$min = $arr[$i];
-		$minindex = $i;
+		$min = $arr[$i];// 数组最小的元素
+        $minIndex = $i;// 数组最小的元素下标
 		for ($j=$i+1; $j <count($arr) ; $j++) { 
 			if ($arr[$j]<$min) {
-				$min = $arr[$j];
-				$minindex = $j;
+				$min = $arr[$j];// 找出最小的元素
+                $minIndex = $j;// 最小元素的下标
 			}
 		}
-		$tmp = $arr[$i];
-		$arr[$i] = $min; // 之前的$arr[$i]要重新赋值
-		$arr[$minindex] = $tmp;	// 之前的$arr[$j]也要重新赋值;
+		list($arr[$i],$arr[$minIndex]) = [$min,$arr[$i]];
+//		$tmp = $arr[$i];
+//		$arr[$i] = $min; // 之前的$arr[$i]要重新赋值
+//		$arr[$minIndex] = $tmp;	// 之前的$arr[$j]也要重新赋值;
 	}
 }
 
